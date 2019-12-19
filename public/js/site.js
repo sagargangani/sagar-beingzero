b7a6c6be359ce736512625c2004f35d0d0ec5032
sagargangani <ganganisagar33@gmail.com>
2019-12-19 15:40:53 +0530

    new new modified

---
+++ public/js/site.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	$.getJSON("/data", function(d){
+		//alert(JSON.stringify(d));
+		$("#name").text(d.name);
+		$("#college").text(d.college);
+		$("#regno").text(d.regno);
+	})
+}}
